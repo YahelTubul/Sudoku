952e53ff595ca0da1522e2994221d450054fa739
Yahel Tubul <yaeltobol07@gmail.com>
2026-01-25 10:23:01 +0200

    add function that check if the number is appear in the row this help to know where place the number

--- Sudoku/Sudoku/Validation.cs
+++ Sudoku/Sudoku/Validation.cs
@@ -3,4 +3,13 @@ namespace Sudoku;
 public class Validation
 {
     public const int SIZE = 9;
+    
+    public static bool AppearInRow(int[][] board, int row, int number){
+        for (int col = 0; col < SIZE; col++)
+        {
+            if (board[row][col] == number)
+                return true;
+        }
+        return false;
+    }
 }
