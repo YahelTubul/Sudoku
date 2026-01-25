af3f04c892e9b60ceb7f85af370448a3d4a13cfa
Yahel Tubul <yaeltobol07@gmail.com>
2026-01-25 10:40:07 +0200

    add function that check if the number is appear in te block by using 2 for loops

--- Sudoku/Sudoku/Validation.cs
+++ Sudoku/Sudoku/Validation.cs
@@ -3,6 +3,7 @@ namespace Sudoku;
 public class Validation
 {
     public const int SIZE_BOARD = 9;
+    public const int SIZE_BLOCK = 3;
     
     public static bool AppearInRow(int[][] board, int row, int number){
         for (int col = 0; col < SIZE_BOARD; col++)
@@ -19,6 +20,21 @@ public class Validation
         {
             if(board[row][col] == number)
                 return true;
+        }
+        return false;
+    }
+
+    public static bool AppearInBlock(int[][] board, int row, int col, int number)
+    {
+        for (int i = 0; i < SIZE_BLOCK; i++)
+        {
+            for (int j = 0; j < SIZE_BLOCK; j++)
+            {
+                if (board[i + row][col + j] == number)
+                {
+                    return true;
+                }
+            }
         }
         return false;
     }
