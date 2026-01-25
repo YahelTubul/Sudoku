ffda5b0e42598386af8f103ddfa510e50ae5dcc6
Yahel Tubul <yaeltobol07@gmail.com>
2026-01-25 10:28:53 +0200

    add function that check if the number is appear in the col, and change the name of the constant variable because this define the board size and I need to define another for to the boxes in big board

--- Sudoku/Sudoku/Validation.cs
+++ Sudoku/Sudoku/Validation.cs
@@ -2,12 +2,22 @@ namespace Sudoku;
 
 public class Validation
 {
-    public const int SIZE = 9;
+    public const int SIZE_BOARD = 9;
     
     public static bool AppearInRow(int[][] board, int row, int number){
-        for (int col = 0; col < SIZE; col++)
+        for (int col = 0; col < SIZE_BOARD; col++)
         {
             if (board[row][col] == number)
+                return true;
+        }
+        return false;
+    }
+
+    public static bool AppearInCol(int[][] board, int col, int number)
+    {
+        for (int row = 0; row < SIZE_BOARD; row++)
+        {
+            if(board[row][col] == number)
                 return true;
         }
         return false;
