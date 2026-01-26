cc42f086c01cb75ff36a8f48329a04401fdbc131
Yahel Tubul <yaeltobol07@gmail.com>
2026-01-26 09:51:37 +0200

    add function that check if the board shapes is valid

--- Sudoku/Sudoku/Validation.cs
+++ Sudoku/Sudoku/Validation.cs
@@ -2,6 +2,25 @@ namespace Sudoku;
 
 public class Validation
 {
-    public const int SIZE_BOARD = 9;
-    public const int SIZE_BLOCK = 3;
+    public const int SizeBoard = 9;
+    public const int SizeBlock = 3;
+
+    private static bool BoardValid(int[][] board, out string errorMsg)
+    {
+        errorMsg = "";
+        if (board == null || board.Length != SizeBlock)
+        {
+            errorMsg = "Board size need to include 9 rows!";
+            return false;
+        }
+
+        for (int row = 0; row < SizeBoard; row++ )
+        {
+            if (board[row] == null || board[row].Length != SizeBlock)
+            {
+                errorMsg = "Board size need to include 9 columns!";
+            }
+        }
+        return true;
+    }
 }
