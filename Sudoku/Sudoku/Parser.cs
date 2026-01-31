23b9bab9ef0127b288b34cf1ea07c8bbfe079220
Yahel Tubul <yaeltobol07@gmail.com>
2026-01-31 16:28:21 +0200

    complete the functions valid input and adding comments and documentation to the function

--- Sudoku/Sudoku/Parser.cs
+++ Sudoku/Sudoku/Parser.cs
@@ -4,10 +4,17 @@ public class Parser
 {
     public const int SizeBoard = 9;
     public const int NumOfCells = 81;
-
+    
+    /// <summary>
+    /// This function check the received input
+    /// </summary>
+    /// <param name="strBoard"></param>
+    /// <param name="errorMsg"></param>
+    /// <returns>true/false, error message</returns>
     private static bool ValidInput(string strBoard, out string errorMsg)
     {
         errorMsg = "";
+        char ch = ' ';
 
         if (strBoard == null)
         {
@@ -16,5 +23,24 @@ public class Parser
         }
         //remove whitespaces from the string 
         strBoard = strBoard.Trim();
+        
+        // check if the length of the string is equal to the number of cells
+        if (strBoard.Length != NumOfCells)
+        {
+            errorMsg = $"The string must contain {NumOfCells} numbers,you need to add {NumOfCells - strBoard.Length} numbers!";
+            return false;
+        }
+        
+        //pass on each char in the array and check if its digit between '0' to '9'
+        for (int i = 0; i < NumOfCells; i++)
+        {
+            ch = strBoard[i];
+            if (ch < '0' || ch > '9')
+            {
+                errorMsg = $"Invalid character in position {i}";
+                return false;
+            }
+        }
+        return true;
     }
 }
