0c5e8f0de66a3bea412f73a6800f87213c860afc
Yahel Tubul <yaeltobol07@gmail.com>
2026-01-31 12:48:11 +0200

    after checking I change the range in the function value valid

--- Sudoku/Sudoku/Validation.cs
+++ Sudoku/Sudoku/Validation.cs
@@ -8,7 +8,7 @@ public class Validation
     private static bool BoardValid(int[][] board, out string errorMsg)
     {
         errorMsg = "";
-        if (board == null || board.Length != SizeBlock)
+        if (board == null || board.Length != SizeBoard)
         {
             errorMsg = "Board size need to include 9 rows!";
             return false;
@@ -16,9 +16,10 @@ public class Validation
 
         for (int row = 0; row < SizeBoard; row++ )
         {
-            if (board[row] == null || board[row].Length != SizeBlock)
+            if (board[row] == null || board[row].Length != SizeBoard)
             {
                 errorMsg = "Board size need to include 9 columns!";
+                return false;
             }
         }
         return true;
@@ -31,7 +32,7 @@ public class Validation
         value = board[row][col];
         
         //check if the value of digit is between 1 to 9
-        if (value < 1 || value > 9)
+        if (value < 0 || value > 9)
         {
             errorMsg = "The value must be between 1 to 9!";
             return false;
@@ -46,27 +47,27 @@ public class Validation
         return true;
     }
 
-    private static bool AddValid(int[][] board, int[] maskRow, int[] maskCol, int[] maskBlock, int row, int col,
+    private static bool AddValid(int[] maskRow, int[] maskCol, int[] maskBlock, int row, int col,
         int block, int value, int bit, out string errorMsg)
     {
         errorMsg = "";
         
         //check if the digit appear in the row
-        if (!SetBit(maskRow[row], bit))
+        if (SetBit(maskRow[row], bit))
         {
             errorMsg = $"There is a duplicate value in {row+1}";
             return false;
         }
         
         //check if the digit appear in the col
-        if (!SetBit(maskCol[col], bit))
+        if (SetBit(maskCol[col], bit))
         {
             errorMsg = $"There is a duplicate value in {col+1}";
             return false;
         }
         
         //check if the digit appear in the block
-        if (!SetBit(maskBlock[block], bit))
+        if (SetBit(maskBlock[block], bit))
         {
             errorMsg = $"There is a duplicate value in {block+1}";
             return false;
@@ -85,6 +86,6 @@ public class Validation
 
     private static int GetBlockIndex(int row, int col)
     {
-        return (row / SizeBoard) * SizeBoard + (col / SizeBoard);
+        return (row / SizeBlock) * SizeBlock + (col / SizeBlock);
     }
 }
