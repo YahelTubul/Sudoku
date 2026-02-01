05d3f581562a03683cdf97b0c5619b3d8024c018
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-01 09:40:17 +0200

    create findCell function that check for the cell with minimum option and adding documintation to the createMask function

--- Sudoku/Sudoku/Solver.cs
+++ Sudoku/Sudoku/Solver.cs
@@ -5,27 +5,40 @@ public class Solver
     private int[] maskRow = new int[Helper.SizeBoard];
     private int[] maskCol = new int[Helper.SizeBoard];
     private int[] maskBlock =  new int[Helper.SizeBoard];
-
+    
+    /// <summary>
+    /// This function turn the board to mask for fast scan
+    /// </summary>
+    /// <param name="board"></param>
     private void CreateMask(int[][] board)
     {
+        //reset each mask before start new board 
         Array.Clear(maskRow, 0, maskRow.Length);
         Array.Clear(maskCol, 0, maskCol.Length);
         Array.Clear(maskBlock, 0, maskBlock.Length);
-
+        
+        //these loops pass on each cell
         for (int row = 0; row < maskRow.Length; row++)
         {
             for (int col = 0; col < maskCol.Length; col++)
             {
+                //save the value in the present cell
                 int value = board[row][col];
                 if (value == 0)
                     continue;
+                
                 int bit = Helper.NumberToBit(value);
                 int indexBlock = Helper.GetBlockIndex(row, col);
+                //sign the number by turn on the bits in the masks
                 maskRow[row] |= bit;
                 maskCol[col] |= bit;
                 maskBlock[indexBlock] |= bit;
             }
         }
     }
-    
+
+    private bool FindCell(int[][] board, out int row, out int col, out int mask)
+    {
+        return true;
+    }
 }
