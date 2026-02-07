c38f2c4c589e051baec99e5898e9aa0f0ffe0725
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-07 17:21:39 +0200

    create test function that validate empty board

--- Sudoku/Sudoku.Tests/ValidationTests.cs
+++ Sudoku/Sudoku.Tests/ValidationTests.cs
@@ -3,5 +3,10 @@ namespace Sudoku.Tests;
 public class ValidationTests
 {
     [Fact]
-    
+    public void Validate_EmptyBoard()
+    {
+        //create validation object
+        var validate = new Validation();
+        
+    }
 }
