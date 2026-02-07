1942168fc4f8c8052e844767d10e50e638100d1a
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-07 14:51:15 +0200

    create folder for unit test in the project

---
+++ Sudoku/Sudoku.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Sudoku.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
