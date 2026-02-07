e8e3375738a1f3500e4a05c5d2cdce8d7467c5c3
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-07 17:06:20 +0200

    add 2 test , parse on null input and short string

--- Sudoku/Sudoku.Tests/ParserTests.cs
+++ Sudoku/Sudoku.Tests/ParserTests.cs
@@ -32,7 +32,39 @@ public class ParserTests
         Assert.True(result);
         Assert.NotNull(board);
         Assert.Equal(0, board[0][0]);
-
-
     }
+
+    [Fact]
+    //This test try do parsing on null that present empty board
+    public void Parse_Null()
+    {
+        // create parser object
+        var parser = new Parser();
+        //call to the parse function from the class
+        bool result = parser.Parse(null, out int[][] board, out string errorMsg);
+        Assert.False(result);
+        Assert.Null(board);
+        Assert.Equal("The string is empty", errorMsg);
+    }
+    
+    [Fact]
+    // This test try to do parsing on short string 
+    public void Parse_ShortBoard()
+    {
+        // create parser object
+        var parser = new Parser();
+        string strBoard = "12345";
+        //call to the parse function from the class
+        bool result = parser.Parse(strBoard, out int[][] board, out string errorMsg);
+        Assert.False(result);
+        Assert.Null(board);
+        Assert.Contains("must contain 81 numbers", errorMsg);
+    }
+
+    [Fact]
+    public void Parse_InvalidInput()
+    {
+        var parser = new Parser();
+    }
+    
 }
