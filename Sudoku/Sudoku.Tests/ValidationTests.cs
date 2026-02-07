28ff6f226b92638f0c885922fe62ae02f4f5b5cd
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-07 17:29:52 +0200

    create test that validate duplicate values in column

--- Sudoku/Sudoku.Tests/ValidationTests.cs
+++ Sudoku/Sudoku.Tests/ValidationTests.cs
@@ -10,6 +10,7 @@ public class ValidationTests
         var validate = new Validation();
         //create empty board
         int[][] board = Helper.CreateEmptyBoard();
+        //call to the validate function from the class
         bool result = validate.Validate(board, out string errorMsg);
         Assert.True(result);
         Assert.Empty(errorMsg);
@@ -21,5 +22,20 @@ public class ValidationTests
     {
         //create validation object
         var validate = new Validation();
+        //create empty board
+        int[][] board = Helper.CreateEmptyBoard();
+        //allocate duplicate values in diffrent rows
+        board[0][0] = 5;
+        board[0][1] = 5;
+        //call to the validate function from the class
+        bool result = validate.Validate(board, out string errorMsg);
+        Assert.False(result);
+        Assert.Contains("duplicate", errorMsg.ToLower());
+    }
+
+    [Fact]
+    public void Validate_DupInCol()
+    {
+        
     }
 }
