8f09214015fd53d85975203f17963cb31e2a3769
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-07 17:58:58 +0200

    create function that check if the sudoku is solved already

--- Sudoku/Sudoku.Tests/SolverTests.cs
+++ Sudoku/Sudoku.Tests/SolverTests.cs
@@ -8,5 +8,19 @@ public class SolverTests
         parser.Parse(strBoard, out int[][] board, out _);
         return board;
     }
+    
+
     [Fact]
+    public void Solve_EasySudoku()
+    {
+        //create solver object
+        var solver = new Solver();
+        string strBoard = "301086504046521070500000001400800002080347900009050038004090200008734090007208103";
+        //call to the parse function
+        int [][] board = ParseBoard(strBoard);
+        bool result = solver.Solve(board, out string errorMsg);
+        
+        
+
+    }
 }
