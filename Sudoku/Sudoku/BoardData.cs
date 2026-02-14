63b28798f8cd451009cc9c4138c38becfe58cf3b
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-14 12:04:00 +0200

    create a get function for each attribute

--- Sudoku/Sudoku/BoardData.cs
+++ Sudoku/Sudoku/BoardData.cs
@@ -2,5 +2,20 @@ namespace Sudoku;
 
 public class BoardData
 {
+    /// <summary>
+    /// size of the board 
+    /// </summary>
+    public int SizeBoard { get; }
+    /// <summary>
+    /// size of the block in board 
+    /// </summary>
+    public int SizeBlock { get; }
+    /// <summary>
+    /// The num of cells in the board 
+    /// </summary>
+    public int NumOfCells { get; }
+    
+    public int CompleteMask { get; }
+    
     
 }
