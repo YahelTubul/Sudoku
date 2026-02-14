25daffa9b58d49a766af002f70d9281e904afda7
Yahel Tubul <yaeltobol07@gmail.com>
2026-02-14 12:10:22 +0200

    create constructor function for BoardData class

--- Sudoku/Sudoku/BoardData.cs
+++ Sudoku/Sudoku/BoardData.cs
@@ -14,8 +14,19 @@ public class BoardData
     /// The num of cells in the board 
     /// </summary>
     public int NumOfCells { get; }
-    
+    /// <summary>
+    /// return the mask that presting all the valid digits in the board
+    /// </summary>
     public int CompleteMask { get; }
+    
+    /// <summary>
+    /// Constructor thar initialize each attribute
+    /// </summary>
+    /// <param name="board"></param>
+    public BoardData(int[][] board)
+    {
+        
+    }
     
     
 }
